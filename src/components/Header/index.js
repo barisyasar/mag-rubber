cbfc098ea23309c83754763b5f9b2d89cf3998ac
Nurullah <barisyasar971@gmail.com>
2022-09-15 12:30:24 +0300

    header updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,21 @@
 import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+
+const links = ["about us", "products", "download", "contact"];
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav className="d-flex justify-content-between align-items-center">
+        <div>
+          {" "}
+          <Link to="/">
+            <img src="/assets/images/navBrand.png" alt="" width="150" />
+          </Link>
+        </div>
+        <div>links</div>
+      </nav>
+    </header>
+  );
 }

52c3306fff7e2d9315d99ab3ef73921b0a035223
azebilir <azebiliraze@example.com>
2022-09-15 17:15:16 +0300

    slider edit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Slider from "../components/Slider";
 export default function Home() {
-  return <Layout>Home</Layout>;
+  return <Layout>
+    Home
+<Slider/>
+
+  </Layout>;
 }

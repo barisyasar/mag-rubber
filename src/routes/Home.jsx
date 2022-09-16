208a28939302aefc6bac003fe299a511efe0e290
azebilir <azebiliraze@example.com>
2022-09-16 16:54:17 +0300

    adding carousel and custom text section

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Layout from "../components/Layout";
+
 import Slider from "../components/Slider";
+import CustomCarousel from "../components/CustomCarousel";
+import CustomText from "../components/CustomText";
 
 export default function Home() {
   return (
     <Layout>
       <Slider />
+      <CustomCarousel />
+      <CustomText />
     </Layout>
   );
 }

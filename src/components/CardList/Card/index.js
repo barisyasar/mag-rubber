15ce98f7abfececdeaec33b4b4359a7c76b92c37
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2022-09-20 17:10:45 +0300

    card component ended

--- src/components/CardList/Card/index.js
+++ src/components/CardList/Card/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Card() {
-  return <div>card</div>;
+export default function Card(props) {
+  const { text } = props;
+  return (
+    <div className="card d-flex justify-content-center">
+      <div className="triangle"></div>
+      <div className="triangle"></div>
+      <div className="triangle"></div>
+      <p className="card-text">{text}</p>
+    </div>
+  );
 }

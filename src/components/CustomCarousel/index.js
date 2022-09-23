603716f81aadca68885a5b10a9e3df307d95c89b
azebilir <azebiliraze@example.com>
2022-09-23 10:04:23 +0300

    carousel svg resimler eklendi

--- src/components/CustomCarousel/index.js
+++ src/components/CustomCarousel/index.js
@@ -26,82 +26,82 @@ const responsive = {
 const carouselItems = [
   {
     id: 1,
-    name: 'product 1',
-    image: 'customCarousel1.jpeg',
+    name: 'SPHERICAL ROLLER BEARINGS ',
+    image: '1.svg',
   },
   {
     id: 2,
-    name: 'product 2',
+    name: 'PULLEYS    ',
     image: 'customCarousel2.jpeg',
   },
   {
     id: 3,
-    name: 'product 3',
+    name: 'PRE-CLEANER GROUP ASSEMBLIES    ',
     image: 'customCarousel3.jpeg',
   },
   {
     id: 4,
-    name: 'product 4',
+    name: 'OPERATOR SEATS     ',
     image: 'customCarousel4.jpeg',
   },
   {
     id: 5,
-    name: 'product 5',
+    name: 'SEALS',
     image: 'customCarousel5.jpeg',
   },
   {
     id: 6,
-    name: 'product 6',
+    name: 'DAMPER',
     image: 'customCarousel6.jpeg',
   },
   {
     id: 7,
-    name: 'product 7',
+    name: 'MOUNTS     ',
     image: 'customCarousel7.jpeg',
   },
   {
     id: 8,
-    name: 'product 8',
+    name: 'FUEL CAPS    ',
     image: 'customCarousel8.jpeg',
   },
   {
     id: 9,
-    name: 'product 9',
+    name: 'PIN SEALS    ',
     image: 'customCarousel9.jpeg',
   },
   {
     id: 10,
-    name: 'product 10',
+    name: 'VIBRATION MOUNTS',
     image: 'customCarousel10.jpeg',
   },
   {
     id: 11,
-    name: 'product 11',
+    name: 'MOTOR GRADERS INSERTS    ',
     image: 'customCarousel11.jpeg',
   },
   {
     id: 12,
-    name: 'product 12',
+    name: 'ENGINE GASKETS    ',
     image: 'customCarousel12.jpeg',
   },
   {
     id: 13,
-    name: 'product 13',
+    name: 'BUSHINGS    ',
     image: 'customCarousel13.jpeg',
   },
   {
     id: 14,
-    name: 'product 14',
+    name: 'KITS    ',
     image: 'customCarousel14.jpeg',
   },
   {
     id: 15,
-    name: 'product 15',
+    name: 'PUMPS    ',
     image: 'customCarousel15.jpeg',
   },
   {
     id: 16,
-    name: 'product 16',
+    name: 'FANS    ',
     image: 'customCarousel16.jpeg',
   },
 ];
@@ -112,12 +112,13 @@ function CustomCarousel() {
       className="category-carousel-item owl-carousel-item filterable-item oleodinamica_2mp"
       data-bs-interval={10000}
     >
+      {/* img tagi src '/sadfasdf.svg' */}
       <a
         alt="Visualizza prodotti oleodinamici MONOBLOCCHI CETOP"
         href="prodotti\monoblocchi-cetop.html"
       >
         <div className="icon">
-          <img src={'/assets/images/' + item.image} />
+          <img src="/assets/images/1.svg"/>
         </div>
 
         <div className="item-position-container mt-3 mb-3 position-relative">
@@ -144,34 +145,20 @@ function CustomCarousel() {
       <div className="wrapper">
         <div className="content_page overflow-hidden index_content">
           <section id="home_categories">
-            <div className="services-div">
-              <div className="faded-text light">
-                <div className="f-text">
-                  <marquee
-                    id="my-text"
-                    className="marquee"
-                    scrollamount="8"
-                    behavior="alternate"
-                  >
-                    Time to Discover
-                  </marquee>
-                </div>
-                <div className="product-triangle">
-                  <img
-                    className="triangle-img"
-                    src="/assets/images/mag-triangle-down.png"
-                  />
-                  <h2 className="margin-60">Product Range</h2>
-                </div>
-              </div>
-            </div>
             <div
               id="home-categories-carousel"
               className="categories-carousel owl-carousel owl-theme filterable-items"
             >
               {items2}
             </div>
-          </section>
+           <div className='owl-button-next'>
+           <div>
+			<span class="m_scroll_arrows next-one"></span>
+			<span class="m_scroll_arrows next-two"></span>
+			<span class="m_scroll_arrows next-three"></span>
+		 </div>
+           </div>
+         </section>
         </div>
       </div>
     </React.Fragment>

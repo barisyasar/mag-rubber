011b11159fa2d7b11e788f523dfda71784d8bd95
azebilir <azebiliraze@example.com>
2022-09-27 16:08:58 +0300

    some css editing

--- src/components/ImageAnimation/index.js
+++ src/components/ImageAnimation/index.js
@@ -1,49 +1,54 @@
-import React from 'react'
+import React from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-import "../../assets/css/components/ImageAnimation/style.css"
+import '../../assets/css/components/ImageAnimation/style.css';
 
 
 export default function ImageAnimation() {
   // ..
-AOS.init();
+  AOS.init();
   return (
-    <div className='container animation-container'> 
-    <div className='row'>
-    <div className='col'>
-      <div className='fade-right'
-      data-aos="fade-right"
-      data-aos-offset="200"
-      data-aos-delay="50"
-      data-aos-duration="2000"
-      data-aos-easing="ease-in-out"
-      data-aos-mirror="false"
-      data-aos-once="false"
-      data-aos-anchor-placement="top-center">
-      <img className='image-right'  src='/assets/images/about-01.png'/>
+    <div className="container animation-container">
+      <div className="row">
+        <div className="col-8">
+          <div
+            className="fade-right"
+            data-aos="fade-right"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="2000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="false"
+            data-aos-once="false"
+            data-aos-anchor-placement="top-center"
+          >
+            <img className="image-right" src="/assets/images/about-01.png" />
+          </div>
+
+          <div
+            className="fade-left"
+            data-aos="fade-left"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="2000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="false"
+            data-aos-once="false"
+            data-aos-anchor-placement="top-center"
+          >
+            <img className="image-left" src="/assets/images/about-01.png" />
+          </div>
+        </div>
+        <div className="col-4 ">
+          <p className='animation-text'>
+            {' '}
+            Our Company Gökçek A.Ş which was established in 1968, has become a
+            well known brand in Turkey with the basic principle of achieving
+            quality in production, marketing and punctual and exact delivery of
+            industrial rubber products…
+          </p>
+        </div>
       </div>
-   
-   
-   <div 
-   className='fade-left'
-   data-aos="fade-left"
-      data-aos-offset="200"
-      data-aos-delay="50"
-      data-aos-duration="2000"
-      data-aos-easing="ease-in-out"
-      data-aos-mirror="false"
-      data-aos-once="false"
-      data-aos-anchor-placement="top-center">
-      <img className='image-left' src='/assets/images/about-01.png'/>
     </div>
-   </div>
-   <div className='col'> <p>  Our Company Gökçek A.Ş which was established in 1968, has become a
-              well known brand in Turkey with the basic principle of achieving
-              quality in production, marketing and punctual and exact delivery
-              of industrial rubber products…</p></div>
-    </div>      
-  
-    
-</div>
-  )
+  );
 }
